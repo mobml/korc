5ed6a75e62b97e3ac4d8b368d91fd85efdd73090
Oscar Murrieta <oscarmlmurrieta@gmail.com>
2026-04-30 17:00:31 -0600

    chore: add database configuration   - add database configuration.   - add docker compose file with the specifications for the db.   - add pydotenv library to load env  variablefrom an .env file.

--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('MYSQL_PORT'),
     }
 }
 
