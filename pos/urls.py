88133e6327a426801d9440e2926a09236023dee4
Oscar Murrieta <oscarmlmurrieta@gmail.com>
2026-05-11 21:28:46 -0600

    fix: modify path to cancel a sale

--- pos/urls.py
+++ pos/urls.py
@@ -6,6 +6,6 @@ BASE_URL = 'api/v1/'
 urlpatterns = [
     path(f'{BASE_URL}products/', views.product_list_api, name='product_list_api'),
     path(f'{BASE_URL}sales/', views.sale_create_api, name='sale_create_api'),
-    path(f'{BASE_URL}sales/<int:sale_id>/', views.sale_cancel_api, name='sale_cancel_api'),
+    path(f'{BASE_URL}sales/<int:sale_id>/cancel/', views.sale_cancel_api, name='sale_cancel_api'),
     path(f'{BASE_URL}sales/<int:sale_id>/ticket/', views.ticket_get_details_api, name='ticket_get_details_api')
 ]
