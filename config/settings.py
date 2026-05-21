8d4416a48f48507ce0e25be9b8c09e824261ed72
Oscar Murrieta <oscarmlmurrieta@gmail.com>
2026-05-20 20:34:58 -0600

    fix: change MySQL to PostreSQL   - modify settings in config to accept PostgreSQL   - delete code related to MySQL drive in config/__init__.py   - modify docker-compose.yml   - add PostgreSQL driver   - fix docker-compose.yml, a grammer error in the variable declaration

--- config/settings.py
+++ config/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("MYSQL_DATABASE"),
-        "USER": os.getenv("MYSQL_USER"),
-        "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("MYSQL_PORT"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
