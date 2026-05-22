4b76d050152fa5ce95d0109da2290d7b187e44e9
Oscar Murrieta <oscarmlmurrieta@gmail.com>
2026-05-21 19:21:18 -0600

    chore: change user manager

--- account/models.py
+++ account/models.py
@@ -1,6 +1,33 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 import uuid
+
+
+class UserManager(BaseUserManager):
+    """Custom user manager"""
+
+    def create_user(self, email: str, password: str | None = None, **kwargs):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+
+        if not password:
+            raise ValueError("Password is required")
+
+        user.save()
+        return user
+
+    def create_superuser(self, email: str, password: str | None = None, **kwargs):
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault(
+            "is_superuser", True
+        )  # all control, this could change, but is good for now
+        kwargs.setdefault("role", User.Role.OWNER)
+
+        return self.create_user(email, password, **kwargs)
 
 
 class User(AbstractUser):
@@ -24,3 +51,5 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
